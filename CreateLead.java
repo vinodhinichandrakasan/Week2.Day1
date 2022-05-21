b221b1817e163da8e34afe09797a65f7bf10d70f
vinodhinichandrakasan <105554381+vinodhinichandrakasan@users.noreply.github.com>
2022-05-21 19:38:59 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,44 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 WebDriverManager.chromedriver().setup();
+		
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+		 driver.manage().window().maximize();
+		 driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		 driver.findElement(By.id("password")).sendKeys("crmsfa");
+		 driver.findElement(By.className("decorativeSubmit")).click();
+		 driver.findElement(By.linkText("CRM/SFA")).click();
+		 driver.findElement(By.linkText("Leads")).click();
+		 driver.findElement(By.linkText("Create Lead")).click();
+		 driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Dedalus");
+		 driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vino");
+		 driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Chan");
+		 driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		 driver.findElement(By.name("description")).sendKeys("Create Lead");
+		 driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("vinodhinichandrakasan@gmail.com");
+		 WebElement ele1 = driver.findElement(By.name("generalStateProvinceGeoId"));
+		 Select city=new Select(ele1);
+		 city.selectByVisibleText("New York");
+			/*
+			 * WebElement dropdown =
+			 * driver.findElement(By.id("createLeadForm_dataSourceId")); Select obj=new
+			 * Select(dropdown); obj.selectByValue("LEAD_CONFERENCE"); driver.close();
+			 */		 
+		 driver.findElement(By.className("smallSubmit")).click();
+		 System.out.println(driver.getTitle());
+		 driver.close();
+	}
+
+}
---
+++ EditLead.java
@@ -0,0 +1,45 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+		 driver.manage().window().maximize();
+		 driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		 driver.findElement(By.id("password")).sendKeys("crmsfa");
+		 driver.findElement(By.className("decorativeSubmit")).click();
+		 driver.findElement(By.linkText("CRM/SFA")).click();
+		 driver.findElement(By.linkText("Leads")).click();
+		 driver.findElement(By.linkText("Create Lead")).click();
+		 driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Dedalus");
+		 driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vino");
+		 driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Chan");
+		 driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		 driver.findElement(By.name("description")).sendKeys("Create Lead");
+		 driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("vinodhinichandrakasan@gmail.com");
+		 WebElement ele1 = driver.findElement(By.name("generalStateProvinceGeoId"));
+		 Select city=new Select(ele1);
+		 city.selectByVisibleText("New York");
+		 driver.findElement(By.className("smallSubmit")).click();
+		 driver.findElement(By.linkText("Edit")).click();
+		 driver.findElement(By.id("updateLeadForm_description")).clear();
+		 driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("New test notes are added");
+		 Thread.sleep(2000);
+		 driver.findElement(By.className("smallSubmit")).click();
+		 System.out.println(driver.getTitle());
+		 driver.close();
+
+	}
+
+}
